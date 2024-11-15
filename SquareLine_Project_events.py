65a8512b61dc7017122fa1ec7dd292e0fe123b76
Tuoc Mia <tuocmia117@gmail.com>
2024-11-15 21:12:53 +0700

    update

---
+++ SquareLine_Project_events.py
@@ -0,0 +1,160 @@
+
+def turnon(event_struct):
+    return
+
+
+def batden(event_struct):
+    return
+
+
+def device1ON(event_struct):
+    return
+
+
+def light1ON(event_struct):
+    return
+
+
+def device2ON(event_struct):
+    return
+
+
+def light2ON(event_struct):
+    return
+
+
+def rgb1ON(event_struct):
+    return
+
+
+def light3ON(event_struct):
+    return
+
+
+def rgb2ON(event_struct):
+    return
+
+
+def light4ON(event_struct):
+    return
+
+
+def lightOFF8(event_struct):
+    return
+
+
+def lightON8(event_struct):
+    return
+
+
+def change(event_struct):
+    return
+
+
+def rgbcolor1(event_struct):
+    return
+
+
+def changeBrightness1(event_struct):
+    return
+
+
+def rgbBrightness1(event_struct):
+    return
+
+
+def rgbColor1(event_struct):
+    return
+
+
+def rgbColor2(event_struct):
+    return
+
+
+def rgbBrightness2(event_struct):
+    return
+
+
+def lightColor1(event_struct):
+    return
+
+
+def lightBrightness1(event_struct):
+    return
+
+
+def lightColor2(event_struct):
+    return
+
+
+def lightBrightness2(event_struct):
+    return
+
+
+def lightColor3(event_struct):
+    return
+
+
+def lightBrightness3(event_struct):
+    return
+
+
+def lightColor4(event_struct):
+    return
+
+
+def ONRGB111(event_struct):
+    return
+
+
+def turnONrgb1(event_struct):
+    return
+
+
+def turnONrgb2(event_struct):
+    return
+
+
+def thaydoimaul1(event_struct):
+    return
+
+
+def thaydoidosang1(event_struct):
+    return
+
+
+def thaydoidosangl1(event_struct):
+    return
+
+
+def thaymaul2(event_struct):
+    return
+
+
+def thaydosangl2(event_struct):
+    return
+
+
+def thaymaul1(event_struct):
+    return
+
+
+def thaydosangl1(event_struct):
+    return
+
+
+def thaymaul3(event_struct):
+    return
+
+
+def thaydosangl3(event_struct):
+    return
+
+
+def thaymaul4(event_struct):
+    return
+
+
+def thaydosangl4(event_struct):
+    return
+
